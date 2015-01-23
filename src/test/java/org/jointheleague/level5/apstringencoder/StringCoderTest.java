4c79d6b9d3c82ee598af74cdf0e037009e1ef4c1
Stephen Cerruti <scerruti@otabi.com>
2015-01-22 22:05:58 -0800

    Added a test case that uses reflection to test the private method in StringCoder.

--- src/test/java/org/jointheleague/level5/apstringencoder/StringCoderTest.java
+++ src/test/java/org/jointheleague/level5/apstringencoder/StringCoderTest.java
@@ -2,36 +2,53 @@ package org.jointheleague.level5.apstringencoder;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import org.jointheleague.level5.apstringencoder.StringCoder;
 import org.jointheleague.level5.apstringencoder.StringPart;
-
 import org.junit.Test;
 
 public class StringCoderTest {
 
 	@Test
 	public void testDecodeString() {
-		StringCoder coder = new StringCoder("sixtyzipperswerequicklypickedfromthewovenjutebag");
+		StringCoder coder = new StringCoder(
+				"sixtyzipperswerequicklypickedfromthewovenjutebag");
 		ArrayList<StringPart> parts = new ArrayList<StringPart>(4);
 		parts.add(new StringPart(37, 3));
 		parts.add(new StringPart(14, 2));
 		parts.add(new StringPart(46, 2));
 		parts.add(new StringPart(9, 2));
-		
+
 		assertEquals("overeager", coder.decodeString(parts));
 	}
 
 	@Test
+	public void testFindPart() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		StringCoder coder = new StringCoder(
+				"sixtyzipperswerequicklypickedfromthewovenjutebag");
+		StringPart part = new StringPart(37, 3);
+		
+        Class<?>[] cArg = new Class[1];
+        cArg[0] = String.class;
+		Method findPart = StringCoder.class.getDeclaredMethod("findPart", cArg);
+		findPart.setAccessible(true);
+		
+		assertEquals(part, findPart.invoke(coder, "overeager"));
+	}
+
+	@Test
 	public void testEncodeString() {
-		StringCoder coder = new StringCoder("sixtyzipperswerequicklypickedfromthewovenjutebag");
+		StringCoder coder = new StringCoder(
+				"sixtyzipperswerequicklypickedfromthewovenjutebag");
 		ArrayList<StringPart> parts = new ArrayList<StringPart>(4);
 		parts.add(new StringPart(37, 3));
 		parts.add(new StringPart(14, 2));
 		parts.add(new StringPart(46, 2));
 		parts.add(new StringPart(9, 2));
-		
+
 		assertEquals(parts, coder.encodeString("overeager"));
 	}
 
